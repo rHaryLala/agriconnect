3c86255460ee12d8bb2409ab21795a42060429b8
radovoahary <voaharyradoniaina@gmail.com>
2026-08-13 16:37:59 +0300

    Adding ValidationPipe in the entry point(main.ts) of the application

--- agriconnect_backend/src/main.ts
+++ agriconnect_backend/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
+  //Instanciation du module racine
   const app = await NestFactory.create(AppModule);
 
   //Configuration de la doc Swagger
@@ -22,5 +24,18 @@ async function bootstrap() {
   
   await app.listen(3000);
 
+  app.useGlobalPipes(
+      // applique ce pipe à TOUTES les routes de l'application, sans le répéter partout
+    new ValidationPipe({
+       // supprime automatiquement les champs du body qui ne sont pas déclarés dans le DTO
+      whitelist: true,
+       
+      // renvoie une erreur 400 si un champ non déclaré est envoyé (au lieu de le supprimer silencieusement)
+      forbidNonWhitelisted: true,
+      
+      // convertit automatiquement les types (ex: un "123" dans l'URL devient le number 123)
+      transform: true,
+    }),
+  )
 }
 bootstrap();
