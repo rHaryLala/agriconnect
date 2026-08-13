2c1ae1409aa97c94e74e0e0c747618559e48890e
radovoahary <voaharyradoniaina@gmail.com>
2026-08-13 15:26:15 +0300

    Writing the code for server configuration & start-up

--- agriconnect_backend/src/main.ts
+++ agriconnect_backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //Configuration de la doc Swagger
+  const config = new DocumentBuilder()
+  .setTitle('API Ferme')
+  .setDescription('Documentation des endpoints backend')
+  .setVersion('1.0')
+  .addBearerAuth() // permet de tester les routes protégées par JWT directement dans Swagger
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document) //accessible sur api/docs
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  })
+  
+  await app.listen(3000);
+
 }
 bootstrap();
