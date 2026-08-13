bd38ede95fea4d214f29e3783bf33df560786af6
radovoahary <voaharyradoniaina@gmail.com>
2026-08-13 16:52:28 +0300

    Debugging

--- agriconnect_backend/eslint.config.mjs
+++ agriconnect_backend/eslint.config.mjs
@@ -2,7 +2,6 @@
 import eslint from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
