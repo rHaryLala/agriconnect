519eebac6ffcabd7f95fb97c4e8d4299dfc4161f
radovoahary <voaharyradoniaina@gmail.com>
2026-08-17 16:38:16 +0300

    Re-writing the entry-point main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // configuration de la doc Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Ferme')
+    .setDescription('Documentation des endpoints backend')
+    .setVersion('1.0')
+    .addBearerAuth() // permet de tester les routes protégées par JWT directement dans Swagger
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // accessible sur /api/docs
+
+  await app.listen(3000);
 }
 bootstrap();
