423efa8dcb6ef8c33925c551b5d98ed2edb998bb
radovoahary <voaharyradoniaina@gmail.com>
2026-08-19 09:44:33 +0300

    Gathering pieces and specifiying the token's time limit in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'CHANGE_MOI_EN_PRODUCTION',
+      //Délai d'éxpiration du token
+      signOptions: {expiresIn: '7d'},
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
