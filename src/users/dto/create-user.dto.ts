cfaff133fc756538e374ef2f341a291f520b600b
radovoahary <voaharyradoniaina@gmail.com>
2026-08-20 11:29:40 +0300

    User's Module dto finished

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength, IsEnum, IsOptional } from "class-validator";
+import { Role } from "@prisma/client";
+
+ //un Admin PEUT choisir le rôle du compte qu'il crée
+export class CreateUserDto {
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    password: string;
+
+    @IsString()
+    firstname: string;
+
+    @IsString()
+    lastname: string;
+
+    @IsOptional()
+    @IsEnum(Role)// vérifie que la valeur est bien ADMIN, COMPTABLE ou OUVRIER 
+    role?: Role;
+}
