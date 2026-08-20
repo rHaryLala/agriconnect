1037ed0ffe18d0efe38a1d5b1c43754523323bf0
radovoahary <voaharyradoniaina@gmail.com>
2026-08-20 11:22:04 +0300

    Module users en cours

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ExempleController } from './exemple.controller'; // ligne ajoutée
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // charge .env
     PrismaModule,
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController, ExempleController], // ExempleController ajouté ici
   providers: [AppService],
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,21 +1,17 @@
+import 'dotenv/config'; // force le chargement du .env AVANT tout le reste de ce fichier
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-//Integration d'un adapter
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 @Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({ adapter });
+  }
 
-export class PrismaService extends PrismaClient implements OnModuleInit{
-    
-    constructor()
-    {
-        super({adapter}); //Prisma 7 exige cet adapter
-    }
-    
-    async onModuleInit()
-    {
-        await this.$connect();
-    }
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
