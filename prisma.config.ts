000379f9b3ff42a8e4eda09ae99fea3b227db0f6
Simeon <simeonsitrakiniaina@gmail.com>
2026-08-21 09:02:58 +0300

    modification d'erreurs dans le schema prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,8 +4,9 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
-    url: "postgresql://agriconnect:jerosalema666@localhost:5433/agriconnect-db?schema=public",
+    url: process.env.DATABASE_URL || "postgresql://agriconnect:a-changer-en-local@localhost:5433/agriconnect?schema=public",
   },
 });
---
+++ prisma/seed.ts
@@ -0,0 +1,71 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+
+import { config } from "dotenv";
+config;
+
+const connectionString =
+  process.env.DATABASE_URL || "postgresql://agriconnect:a-changer-en-local@localhost:5433/agriconnect?schema=public";
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  console.log( "Données de test en cours d'insertion...");
+
+  await prisma.user.deleteMany();
+  await prisma.farm.deleteMany();
+
+  const farm = await prisma.farm.create({
+    data: {
+      name: "Agriconnect Test1",
+      location: "Sambaina",
+    },
+  });
+
+  console.log(`Ferme créée : ${farm.name} (ID: ${farm.id})`);
+
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        email: "admin@agriconnect.com",
+        password: "password123",
+        firstName: "Simeon",
+        lastName: "Admin",
+        role: "ADMIN",
+        farmId: farm.id,
+      },
+      {
+        email: "comptable@agriconnect.com",
+        password: "password123",
+        firstName: "Lesoa",
+        lastName: "Finance",
+        role: "COMPTABLE",
+        farmId: farm.id,
+      },
+      {
+        email: "ouvrier@agriconnect.com",
+        password: "password123",
+        firstName: "Voahary",
+        lastName: "Terrain",
+        role: "OUVRIER",
+        farmId: farm.id,
+      },
+    ],
+  });
+
+  console.log(`${users.count} utilisateurs insérés !`);
+  console.log("Seeding terminé avec succès !");
+}
+
+main()
+  .catch((e) => {
+    console.error(" Erreur lors de l'insertion :", e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  });
