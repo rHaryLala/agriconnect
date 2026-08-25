6eb24152d5b8462d2089c376c97d2f6c33ee5a62
radovoahary <voaharyradoniaina@gmail.com>
2026-08-25 11:17:50 +0300

    debugging...

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -1,11 +1,20 @@
 import { Injectable, BadRequestException, NotFoundException } from "@nestjs/common";
-import { PrismaService } from "../prisma/prisma.service";
+import { PrismaClient } from '../../generated/prisma/client';
 import { CreateStockDto } from "./dto/create-stock.dto";
 import { UpdateStockDto } from "./dto/update-stock.dto";
 import { CreateStockMovementDto } from "./dto/create-stock-movement.dto";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class StockService {
-    
+      // ---------- StockItem (le "catalogue" des articles) ----------
+    constructor(private prisma: PrismaService) {}
+  
+      async createItem(dto: CreateStockDto, farmId: string) {
+    return this.prisma.stockItem.create({
+      data: { ...dto, farmId },
+    });
+  }
+
 }
 
