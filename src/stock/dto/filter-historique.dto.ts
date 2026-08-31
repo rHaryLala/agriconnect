96a5f2b0560c7d891219af3cea651dfc877fb224
radovoahary <voaharyradoniaina@gmail.com>
2026-08-31 09:29:09 +0300

    Added date filter in filter-historique.dto

--- src/stock/dto/filter-historique.dto.ts
+++ src/stock/dto/filter-historique.dto.ts
@@ -1,12 +1,20 @@
-import { IsOptional, IsUUID, IsEnum } from "class-validator";
-import { MouvementType } from "@prisma/client";
+import { IsOptional, IsUUID, IsEnum, IsDateString } from 'class-validator';
+import { MouvementType } from '../../../generated/prisma';
 
 export class FilterHistoriqueDto {
-    @IsOptional()
-    @IsUUID()
-    itemId?: string;
+  @IsOptional()
+  @IsUUID()
+  itemId?: string;
 
-    @IsOptional()
-    @IsEnum(MouvementType)
-    type?: MouvementType;
+  @IsOptional()
+  @IsEnum(MouvementType)
+  type?: MouvementType;
+
+  @IsOptional()
+  @IsDateString() // filtre : mouvements à partir de cette date
+  dateDebut?: string;
+
+  @IsOptional()
+  @IsDateString() // filtre : mouvements jusqu'à cette date
+  dateFin?: string;
 }
