4d53df5687f38903d537144c52770984121603a2
Simeon <simeonsitrakiniaina@gmail.com>
2026-09-01 14:14:21 +0300

    Mise a jour de la create-transaction.dto.ts

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,36 @@
-export class CreateTransactionDto {}
+import { TransactionType } from "@prisma/client";
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, IsPositive, IsDateString, isNumber } from "class-validator";
+
+export enum transactionType {
+    RECETTE = 'RECETTE',
+    DEPENSE = 'DEPENSE',
+}
+
+export class CreateTransactionDto {
+    @IsNumber()
+    @IsPositive()
+    @IsNotEmpty()
+    amount!: number;
+
+    @IsEnum(TransactionType)
+    @IsNotEmpty()
+    type!: TransactionType;
+
+    @IsString()
+    @IsOptional()
+    description!: String;
+
+    @IsDateString()
+    @IsOptional()
+    date?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    farmId!: string;
+
+    @IsString()
+    @IsOptional()
+    categoryId?: string;
+
+}
+
