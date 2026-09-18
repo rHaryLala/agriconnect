8ee5e75578981bcae1937b634a7983899a48dd12
radovoahary <voaharyradoniaina@gmail.com>
2026-09-18 08:55:31 +0300

    Changing a string to role[] in the roles.guard.ts

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { ROLES_KEY } from "../decorators/roles.decorator";
+import { Role } from "@prisma/client";
 
 @Injectable()
 export class RolesGuard implements CanActivate
@@ -8,7 +9,7 @@ export class RolesGuard implements CanActivate
     constructor(private reflector: Reflector) {}//Reflector pour lire les métadonnées
 
     canActivate(context: ExecutionContext): boolean {
-      const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
         context.getHandler(),
         context.getClass(),
       ]);
