8015d7a9533a2337ee29bec40cc94693352e675d
radovoahary <voaharyradoniaina@gmail.com>
2026-09-18 08:53:56 +0300

    Correcting some mistake in the role decorator.ts

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,9 +1,9 @@
 //Definition de la clé à rechercher
-
+import { Role } from "@prisma/client";
 import { SetMetadata } from "@nestjs/common";
 
 // Une clé unique pour retrouver cette métadonnée plus tard dans le guard 
 // une simple chaîne de caractères, choisie une fois pour toutes.
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
